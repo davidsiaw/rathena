84e89ad719e4bc71416d740825573c778ebeca98
David Siaw <davidsiaw@gmail.com>
2017-11-09 10:59:17 +0900

    remove personal commandline script

--- docker-run.sh
+++
@@ -1,1 +0,0 @@
-docker run -ti -p 6900:6900 -p 6121:6121 -p 5121:5121 --network=rathena --name rathena -v /home/david/rize/client:/data -v /home/david/programs/rathena_sql:/var/lib/mysql -v /home/david/rize/client_static:/patch/content -e SERVER_IP=192.168.1.15 -e SERVER_NAME=Lunacy ra
